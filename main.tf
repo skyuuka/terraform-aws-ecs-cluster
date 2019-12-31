93b673a9a91941a6ec6b61552b3fb3bf162939df
Ada Smaldon <ada.smaldon@bjss.com>
2019-12-31 12:29:08 +0000

    make vpc_security_group_ids a list

--- main.tf
+++ main.tf
@@ -168,7 +168,7 @@ resource "aws_launch_template" "container_instance" {
   instance_initiated_shutdown_behavior = "terminate"
   instance_type                        = var.instance_type
   key_name                             = var.key_name
-  vpc_security_group_ids               = aws_security_group.container_instance.id
+  vpc_security_group_ids               = [aws_security_group.container_instance.id]
   user_data                            = base64encode(data.template_cloudinit_config.container_instance_cloud_config.rendered)
 
   monitoring {
