41f802ac2c41a6c093e96f5f33125acee68ef60a
Ada Smaldon <ada.smaldon@bjss.com>
2020-01-02 10:45:27 +0000

    return to using capitalization since that's the standard format we expect to see in AWS

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-  project                       = lookup(var.tag_map, "project", "Unknown")
-  environment                   = lookup(var.tag_map, "environment", "Unknown")
+  project                       = lookup(var.tag_map, "Project", "Unknown")
+  environment                   = lookup(var.tag_map, "Environment", "Unknown")
   cluster_name                  = "ecs${title(local.environment)}Cluster"
   autoscaling_group_name        = "asg${title(local.environment)}ContainerInstance"
   security_group_name           = "sgContainerInstance"
